c7b39cd395149cfb4b69f126df2554da1e90a67f
Deyong Chen <deyong.chen@amlogic.com>
2023-10-24 20:24:51 +0800

    bpqserver: modify user menu value error [1/1]
    PD#SWPL-143549
    
    Problem:
    user menu value are error
    
    Solution:
    --
    
    Verify:
    T962D4(T5W)
    
    Change-Id: I660a3ff4c0be84f2901577f90c1067d2b81d7be9
    Signed-off-by: Deyong Chen <deyong.chen@amlogic.com>

--- libpq/SSMHeader.cpp
+++ libpq/SSMHeader.cpp
@@ -217,8 +217,6 @@ struct SSMHeader_section2_t gSSMHeader_section2[] = {
     {.addr = 0, .size = SOURCE_MAX, .valid = 0, .rsv = {0}},
     {.addr = 0, .size = SOURCE_MAX, .valid = 0, .rsv = {0}},
     {.addr = 0, .size = SOURCE_MAX, .valid = 0, .rsv = {0}},
-    {.addr = 0, .size = MAX_COLORCUSTOMIZE_CM_PARAM_SIZE, .valid = 0, .rsv = {0}},
-    {.addr = 0, .size = MAX_COLORCUSTOMIZE_3DLUT_PARAM_SIZE, .valid = 0, .rsv = {0}},
     {.addr = 0, .size = 0, .valid = 0, .rsv = {0}},
     {.addr = 0, .size = 0, .valid = 0, .rsv = {0}},
     {.addr = 0, .size = 0, .valid = 0, .rsv = {0}},
@@ -317,9 +315,11 @@ struct SSMHeader_section2_t gSSMHeader_section2[] = {
     {.addr = 0, .size = SOURCE_MAX, .valid = 0, .rsv = {0}},
     {.addr = 0, .size = SOURCE_MAX * PQ_FMT_MAX * VPP_PICTURE_MODE_MAX * MAX_PICTUREMODE_PARAM_SIZE, .valid = 0, .rsv = {0}},
     {.addr = 0, .size = SOURCE_MAX * PQ_FMT_MAX * VPP_PICTURE_MODE_MAX, .valid = 0, .rsv = {0}},
+    {.addr = 0, .size = MAX_COLORCUSTOMIZE_CM_PARAM_SIZE, .valid = 0, .rsv = {0}},
+    {.addr = 0, .size = MAX_COLORCUSTOMIZE_3DLUT_PARAM_SIZE, .valid = 0, .rsv = {0}},
 };
 
-struct SSMHeader_section1_t gSSMHeader_section1 = 
+struct SSMHeader_section1_t gSSMHeader_section1 =
 {
-    .magic = 0x8f8f8f8f, .version = 3449175161, .count = 312, .rsv = {0}
+    .magic = 0x8f8f8f8f, .version = 3449175161, .count = 314, .rsv = {0}
 };
